9d6c0bcce2d258a1a2c4cb2067fbf10f13a857e6
Jessé Snow <jesse.neves@outlook.com>
2021-04-01 18:06:04 -0300

    Show the icons from Openweathermap

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const temperature = document.getElementById("temperature");
 const temperatureFeel = document.getElementById("weather");
-const imgDiv = document.getElementsByClassName("img");
+const imgDiv = document.getElementById("img");
 const submitButton = document.getElementById("submit-input");
 const cityInputText = document.getElementById("city-input");
+let weatherImg = document.getElementById("weatherImg")
 let city = "";
 
 // Input
@@ -10,25 +11,39 @@ submitButton.addEventListener("click",function(){
     city = "";
     city = cityInputText.value;
     let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8542a14a77bcc9d04a6905a0cb7c1756&mode=json&units=metric&lang=pt_br`;
+    
+    // Request data from openweather URL
     fetch(url)
     .then(function(data){
+        // Returns a Object of the JSON url of openweathermap API
         return data.json();
     })
     .then(function(data){
-        console.log(data) // Delete this
+        // Show the temperature degrees
         temperature.innerText = parseInt(data.main.temp) + '°';
         return data;
     })
     .then(function(data){
-        console.log(data); // Delete this
+        // Show the temperature description
         let temperatureDescription = data.weather[0].description;
         let temperatureDescriptionUpperCase = temperatureDescription.replace(temperatureDescription.charAt(0),temperatureDescription.charAt(0).toUpperCase());
         temperatureFeel.innerText = temperatureDescriptionUpperCase;
+        return data;
+    })
+    .then(function(data){
+        // Show the Icon 
+        let iconId = data.weather[0].icon;
+        let icon = `http://openweathermap.org/img/wn/${iconId}@2x.png`;
+        if(weatherImg)
+        {
+            weatherImg.remove();
+        }
+        let newText = document.createElement("img");
+        newText.setAttribute("src",icon);
+        newText.setAttribute("id","weatherImg")
+        imgDiv.insertBefore(newText,temperatureFeel);
     })
     .catch(function(){
         window.alert("Erro, recarregue a página.");
     })
 })
-
-
-
